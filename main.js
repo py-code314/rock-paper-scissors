670fcfb7966a3689f043088d0859f0fc5c116419
Srikanth Kotipalli <mynahomaina@Srikanths-MacBook-Pro.local>
2024-06-21 22:31:53 -0400

    Create function getHumanChoice

--- main.js
+++ main.js
@@ -4,7 +4,22 @@ const choices = ["rock", "paper", "scissors"];
 
 function getComputerChoice(items) {
     let item = items[Math.floor(Math.random() * items.length)]
-    console.log(item)
+    return item
 }
 
-getComputerChoice(choices)
+// getComputerChoice(choices)
+
+function getHumanChoice() {
+    let response = prompt("Pick one from following options: rock, paper, scissors")
+    response = response.toLowerCase()
+    
+    if (response === " ") {
+        return "Field can not be left blank."
+    } else if (response === "rock" || response === "paper" || response === "scissors") {
+        return response
+    } else {
+        return "Enter a valid response"
+    }
+}
+
+// getHumanChoice()
