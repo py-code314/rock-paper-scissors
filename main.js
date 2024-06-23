ffe61cff2fecc5a5d63c483a4aad169e2948ac2e
Srikanth Kotipalli <mynahomaina@Srikanths-MacBook-Pro.local>
2024-06-23 17:08:59 -0400

    Create function playGame

--- main.js
+++ main.js
@@ -1,64 +1,81 @@
-console.log("Hello world")
+let humanScore = 0;
+let computerScore = 0;
 
-let humanScore = 0
-let computerScore = 0
-
-
-
-function getComputerChoice(choiceArray) {
-    let choice = choiceArray[Math.floor(Math.random() * choiceArray.length)]
-    return choice
+function getComputerChoice() {
+    choiceArray = ["rock", "paper", "scissors"];
+    const choice = choiceArray[Math.floor(Math.random() * choiceArray.length)];
+    // console.log(choice)
+    return choice;
 }
 
-// getComputerChoice(choices)
-
 function getHumanChoice() {
-    let response = prompt("Pick one from following options: rock, paper, scissors")
-    response = response.toLowerCase()
-    
+    let response = prompt(
+        "Pick one from following options: rock, paper, scissors"
+    );
+    if (response === null) {
+        alert("You canceled the game.")
+        
+    }
+    //TODO Line below printing Type Error to console.
+    response = response.toLowerCase();
+
     if (response === "") {
-        alert("Field can not be left blank.") 
-    } else if (response === "rock" || response === "paper" || response === "scissors") {
-        return response
+        alert("Field can not be left blank.");
+    } else if (
+        response === "rock" ||
+        response === "paper" ||
+        response === "scissors"
+    ) {
+        return response;
     } else {
-        alert("Enter a valid response")
+        alert("Enter a valid response");
     }
 }
 
-// getHumanChoice()
-
 function playRound(humanChoice, computerChoice) {
-    if (humanChoice === "rock" && computerChoice === "rock" ||
-        humanChoice === "paper" && computerChoice === "paper" ||
-        humanChoice === "scissors" && computerChoice === "scissors") {
-        alert("It's a tie! You both chose same.")
+    if (
+        (humanChoice === "rock" && computerChoice === "rock") ||
+        (humanChoice === "paper" && computerChoice === "paper") ||
+        (humanChoice === "scissors" && computerChoice === "scissors")
+    ) {
+        alert("It's a tie! You both chose same.");
     } else if (humanChoice === "rock" && computerChoice === "paper") {
-        alert("You lose! Paper beats Rock.")
-        computerScore++
+        alert("You lose! Paper beats Rock.");
+        computerScore++;
     } else if (humanChoice === "rock" && computerChoice === "scissors") {
-        alert("You win! Rock beats Scissors.")
-        humanScore++
+        alert("You win! Rock beats Scissors.");
+        humanScore++;
     } else if (humanChoice === "paper" && computerChoice === "rock") {
-        alert("You win! Paper beats Rock.")
-        humanScore++
+        alert("You win! Paper beats Rock.");
+        humanScore++;
     } else if (humanChoice === "paper" && computerChoice === "scissors") {
-        alert("You lose! Scissors beats Paper.")
-        computerScore++
+        alert("You lose! Scissors beats Paper.");
+        computerScore++;
     } else if (humanChoice === "scissors" && computerChoice === "rock") {
-        alert("You lose! Rock beats Scissors.")
-        computerScore++
+        alert("You lose! Rock beats Scissors.");
+        computerScore++;
     } else if (humanChoice === "scissors" && computerChoice === "paper") {
-        alert("You win! Scissors beats Paper.")
-        humanScore++
-    } 
+        alert("You win! Scissors beats Paper.");
+        humanScore++;
+    }
 }
 
-const humanSelection = getHumanChoice()
-// console.log(humanSelection)
-const choices = ["rock", "paper", "scissors"];
-const computerSelection = getComputerChoice(choices)
-// console.log(computerSelection)
+function playGame() {
+    //TODO i is getting incremented even if response is "" or some other value.
+    for (i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection)
-console.log(humanScore)
-console.log(computerScore)
+        playRound(humanSelection, computerSelection);
+    }
+
+    if (humanScore === computerScore) {
+        alert("The game is a tie! Scores are equal")
+    } else if (humanScore > computerScore) {
+        alert("You won! You beat the Computer")
+    } else {
+        alert("You lost! Computer won the game")
+    }
+}
+
+// playGame();
