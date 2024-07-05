922cd7aa37758d9de20bcde0df675b7b2ce019cf
Srikanth K <122169588+py-code314@users.noreply.github.com>
2024-07-04 21:21:38 -0400

    Refactor getHumanChoice() to get response based on user's choice

--- main.js
+++ main.js
@@ -11,27 +11,53 @@ function getComputerChoice() {
 
 // Function to get human choice.
 function getHumanChoice() {
-    let response = prompt(
-        "Pick one from following options: rock, paper, scissors"
-    );
-    if (response === null) {
-        alert("You canceled the game.");
-    }
+    // let response = prompt(
+    //     "Pick one from following options: rock, paper, scissors"
+    // );
+    // if (response === null) {
+    //     alert("You canceled the game.");
+    // }
     
-    response = response.toLowerCase();
+    // response = response.toLowerCase();
 
-    if (response === "") {
-        alert("Field can not be left blank.");
-    } else if (
-        response === "rock" ||
-        response === "paper" ||
-        response === "scissors"
-    ) {
-        return response;
-    } else {
-        alert("Enter a valid response");
-    }
+    // if (response === "") {
+    //     alert("Field can not be left blank.");
+    // } else if (
+    //     response === "rock" ||
+    //     response === "paper" ||
+    //     response === "scissors"
+    // ) {
+    //     return response;
+    // } else {
+    //     alert("Enter a valid response");
+    // }
+    let response;
+    const buttons = document.querySelector(".buttons")
+    // console.log(btn)
+    buttons.addEventListener('click', (event) => {
+        // e.preventDefault()
+        // const value = btn.value
+        // console.log(e)
+        let target = event.target
+        console.log(target.id)
+
+        switch (target.id) {
+            case "rock":
+                response = "rock";
+                break;
+            case "paper":
+                response = "paper";
+                break;
+            case "scissors":
+                response = "scissors"
+                break;
+        }
+    })
+    console.log(response)
+    return response
 }
+
+getHumanChoice()
 
 // Function to play one round of rock-paper-scissors.
 function playRound(humanChoice, computerChoice) {
@@ -56,10 +82,11 @@ function playRound(humanChoice, computerChoice) {
         alert(`You win! ${humanChoice} beats ${computerChoice}.`);
         humanScore++;
     } 
+    
 }
 
 // Function to play five rounds of rock-paper-scissors.
-function playGame() {
+/*function playGame() {
     let i = 0;
     let maxRounds = 5;
     while (i < maxRounds) {
@@ -93,4 +120,4 @@ function playGame() {
     }
 }
 
-playGame();
+playGame();*/
